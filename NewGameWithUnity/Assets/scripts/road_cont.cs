9f375ce8cfbf7bdc9e5868953fe13852947c072b
EmirGaziKopar <75802906+EmirGaziKopar@users.noreply.github.com>
2021-05-17 12:00:18 +0300

    2.6

--- NewGameWithUnity/Assets/scripts/road_cont.cs
+++ NewGameWithUnity/Assets/scripts/road_cont.cs
@@ -26,7 +26,7 @@ public class road_cont : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
 
-        transform.position += new Vector3(transform.GetChild(0).GetComponent<Renderer>().bounds.size.x * 4, 0, 0);
+        transform.position += new Vector3(transform.GetChild(1).GetComponent<Renderer>().bounds.size.x * 4, 0, 0);
         Debug.Log("Yenileme Gerçekleþti");
 
     }
